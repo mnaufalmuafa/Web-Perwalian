7fc83c83ce08588966ce940cbb14adf89a74ef66
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-07-26 20:24:10 +0700

    Membuat layout dashboard admin

--- resources/views/pages/admin/dashboard_admin.blade.php
+++ resources/views/pages/admin/dashboard_admin.blade.php
@@ -7,7 +7,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ url('css/login_admin.css') }}">
+    <link rel="stylesheet" href="{{ url('css/dashboard_admin.css') }}">
     <title>Dashboard Admin</title>
 </head>
 <body>
@@ -16,6 +16,39 @@
         <hr>
     </header>
     <main id="body-main-content">
-        <p>Dashboard Admin</p>
+        <section id="firstSection">
+            <article>
+                <main>
+                    <div>
+                        <h3>11</h3>
+                        <h3>Dosen</h3>
+                    </div>
+                    <img src="/image/icon/png/teacher.png" alt="Dosen">
+                </main>
+                <footer>Lebih Lanjut</footer>
+            </article>
+
+            <article>
+                <main>
+                    <div>
+                        <h3>22</h3>
+                        <h3>Mahasiswa</h3>
+                    </div>
+                    <img src="/image/icon/png/student.png" alt="Mahasiswa">
+                </main>
+                <footer>Lebih Lanjut</footer>
+            </article>
+
+            <article>
+                <main>
+                    <div>
+                        <h3>33</h3>
+                        <h3>Kelas</h3>
+                    </div>
+                    <img src="/image/icon/png/class.png" alt="Kelas">
+                </main>
+                <footer>Lebih Lanjut</footer>
+            </article>
+        </section>
     </main>
 </html>
