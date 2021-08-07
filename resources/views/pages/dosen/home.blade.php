9a7dbf1f01741339bbc71f7d84aea9e19a782518
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-08-07 18:49:49 +0700

    Menambah button form1

--- resources/views/pages/dosen/home.blade.php
+++ resources/views/pages/dosen/home.blade.php
@@ -10,6 +10,7 @@
 
 @section('content')
     <main>
+        <a href="/form1"><button class="default-button-color">Isi form 1</button></a>
         <a href="/form2"><button class="default-button-color">Isi form 2</button></a>
         <a href=""><button class="default-button-color">Isi form 3</button></a>
     </main>
