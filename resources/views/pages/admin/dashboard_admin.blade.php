6b368b82238eb7391ec59dcd02d1f56ed25fa249
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-08-21 14:55:47 +0700

    Membuat tampilan rekap perwalian

--- resources/views/pages/admin/dashboard_admin.blade.php
+++ resources/views/pages/admin/dashboard_admin.blade.php
@@ -44,6 +44,104 @@
                 <footer v-on:click="redirectToInfo('mahasiswa')">Lebih Lanjut</footer>
             </article>
         </section>
+
+        <h2>Rekap Perwalian</h2>
+        <section id="filterSection">
+            <section>
+                <p id="filterTitle">Filter</p> {{-- Semester, Tahun Ajaran, Status --}}
+            </section>
+            <section>
+                <form action="">
+                    <section class="formSection">
+                        <section>
+                            <p>Tahun Ajaran</p>
+                        </section>
+                        <section>
+                            <input type="checkbox" name="" id="ta1">
+                            <label for="ta1">2020/2021</label>
+                            <br>
+                            <input type="checkbox" name="" id="ta2">
+                            <label for="ta2">2021/2022</label>
+                            <br>
+                            <input type="checkbox" name="" id="ta3">
+                            <label for="ta3">2021/2022</label>
+                        </section>
+                    </section>
+
+                    <section class="formSection">
+                        <section>
+                            <p>Semester</p>
+                        </section>
+                        <section>
+                            <input type="checkbox" name="" id="ganjilOpt">
+                            <label for="ganjilOpt">Ganjil</label>
+                            <br>
+                            <input type="checkbox" name="" id="genapOpt">
+                            <label for="genapOpt">Genap</label>
+                        </section>
+                    </section>
+
+                    <section class="formSection">
+                        <section>
+                            <p>Status</p>
+                        </section>
+                        <section>
+                            <input type="checkbox" name="" id="ganjilOpt">
+                            <label for="ganjilOpt">Sudah Mengisi</label>
+                            <br>
+                            <input type="checkbox" name="" id="genapOpt">
+                            <label for="genapOpt">Belum Mengisi</label>
+                        </section>
+                    </section>
+                </form>
+            </section>
+        </section>
+
+        <section id="sortingSection">
+            <label for="">Urutkan berdasarkan : </label>
+            <select>
+                <option>-- Pilih metode pengurutan --</option>
+                <option value="">Tahun Ajaran</option>
+                <option value="">Semester</option>
+                <option value="">Status</option>
+            </select>
+        </section>
+
+        <section id="tableContainer">
+            <table cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Tahun Ajaran</th>
+                        <th>Semester</th>
+                        <th>Kode Dosen</th>
+                        <th>Status</th>
+                        <th>Kelas</th>
+                        <th>Link File</th>
+                        <th>Tanggal Upload</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2020/2021</td>
+                        <td>Genap</td>
+                        <td>ASD</td>
+                        <td>Belum Mengisi</td>
+                        <td>IFX-43-INT</td>
+                        <td><a href="#">https://mail.google.com/mail/u/0/#inbox/FfghMfcgzGkZsxGkjJCgNCFEBEDFNfdgsdSAPpCpLKSlqbkMV</a></td>
+                        <td>23-09-2021 04:32</td>
+                    </tr>
+                    <tr>
+                        <td>2020/2021</td>
+                        <td>Genap</td>
+                        <td>SDF</td>
+                        <td>Sudah Mengisi</td>
+                        <td>IFX-43-INT</td>
+                        <td><a href="#">https://mail.google.com/mail/u/0/#inbox/FfghMfcgzGkZsxGkjJCgNCFEBEDFNfdgsdSAPpCpLKSlqbkMV</a></td>
+                        <td>23-09-2021 04:32</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
     </main>
 @endsection
 
