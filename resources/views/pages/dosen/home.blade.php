afb6c45b0edfab37f1f0193bb29348ce914f2397
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-08-21 15:03:50 +0700

    Menambahkan card pada home dosen

--- resources/views/pages/dosen/home.blade.php
+++ resources/views/pages/dosen/home.blade.php
@@ -10,8 +10,28 @@
 
 @section('content')
     <main>
-        <a href="/form1?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 1</button></a>
-        <a href="/form2?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 2</button></a>
-        <a href="/form3?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 3</button></a>
+    <div class="card">
+            <div class="container">    
+                <h4><b>FORM 1</b></h4>
+                <p>info.......</p>
+                <a href="/form1?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 1</button></a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container">
+                <h4><b>FORM 2</b></h4>
+                <p>info.......</p>
+                <a href="/form2?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 2</button></a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="container">
+                <h4><b>FORM 3</b></h4>
+                <p>info.......</p>
+                <a href="/form3?lecturer_id=0&class_id=0&school_year_id=1&semester=Ganjil"><button class="default-button-color">Isi form 3</button></a>
+            </div>
+        </div>
     </main>
 @endsection
